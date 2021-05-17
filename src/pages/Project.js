b00710963d0ea62d10698afa550fdd66617dd037
adouch9 <mr.ouldmoussa@gmail.com>
2021-05-17 18:50:17 +0200

    exo

---
+++ src/pages/Project.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navigation from '../components/Navigation'
+
+const Project = () => {
+    return (
+        <div>
+            <Navigation />
+            PROJECT
+        </div>
+    )
+}
+
+export default Project
